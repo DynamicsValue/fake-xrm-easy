d9883f18bb45a4dcdb3c6fcf986aa051bcf876ac
jordimontana82 <jordi.montana@gmail.com>
2022-05-28 12:57:41 +0200

    Build for all supported configs

--- build.ps1
+++ build.ps1
@@ -4,18 +4,12 @@ param (
 
 Write-Host "Restoring packages..."
 
-if($targetFrameworks -eq "netcoreapp3.1")
-{
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
-}
-else 
-{
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -targetFrameworks $targetFrameworks
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -targetFrameworks $targetFrameworks
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -targetFrameworks $targetFrameworks
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
-    ./build-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
-}
+
+./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -targetFrameworks $targetFrameworks
+./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -targetFrameworks $targetFrameworks
+./build-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -targetFrameworks $targetFrameworks
+./build-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
+./build-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
+
 
 Write-Host "Done building all configurations :) "
